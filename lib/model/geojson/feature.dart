cd9892abb73f572197d6e5f955a140e6c1f5323f
Johannes Bolz <kontakt@johannes-bolz.de>
2019-05-30 17:58:50 +0200

    LineString + Polygon parsing

--- lib/model/geojson/feature.dart
+++ lib/model/geojson/feature.dart
@@ -7,7 +7,9 @@ class FeatureCollection {
   factory FeatureCollection.fromJson(Map<String, dynamic> json) {
     var featureList = json['features'] as List;
     List<Feature> features = featureList.map((it) => Feature.fromJson(it)).toList();
-    return FeatureCollection(features: features);
+    FeatureCollection featureCollection = FeatureCollection(features: features);
+    featureCollection.features.forEach((feature) => print(feature));
+    return featureCollection;
   }
 }
 
@@ -20,26 +22,48 @@ class Feature {
     switch (type) {
       case 'Point':
         return Point.fromJson(geometry);
+      case 'LineString':
+        return LineString.fromJson(geometry);
+      case 'Polygon':
+        return Polygon.fromJson(geometry);
     }
     return null;
   }
 }
 
 class LineString extends Feature {
-
+  LineString({this.coordinates});
+  List<Coordinates> coordinates;
+  factory LineString.fromJson(Map<String, dynamic> json) {
+    var coordinatesList = json['coordinates'] as List;
+    return LineString(
+      coordinates: coordinatesList.map((it) => Coordinates.fromJson(it)).toList()
+    );
+  }
 }
 
 class Polygon extends Feature {
+  Polygon({this.coordinates});
+  List<List<Coordinates>> coordinates;
+  factory Polygon.fromJson(Map<String, dynamic> json) {
+    var jsonListOfCoordinatesLists = json['coordinates'] as List;
+    var listOfCoordinatesLists = jsonListOfCoordinatesLists.map((coordinatesList) =>
+      (coordinatesList as List).map((jsonCoordinates) =>
+          Coordinates.fromJson(jsonCoordinates)
+      ).toList()
+    ).toList();
 
+    return Polygon(
+        coordinates: listOfCoordinatesLists
+    );
+  }
 }
 
 class Point extends Feature {
   Point({this.coordinates});
   Coordinates coordinates;
   factory Point.fromJson(Map<String, dynamic> json) {
-    var coordinatesFromJson = json['coordinates'];
-    List<num> coordsList = new List<num>.from(coordinatesFromJson);
-    return Point(coordinates: Coordinates(lng: coordsList[0], lat: coordsList[1]));
+    return Point(coordinates: Coordinates.fromJson(json['coordinates']));
   }
 }
 
@@ -47,6 +71,11 @@ class Coordinates {
   Coordinates({this.lng, this.lat});
   double lng;
   double lat;
+  factory Coordinates.fromJson(dynamic json) {
+    print(json);
+    var coordinatesList = new List<num>.from(json);
+    return Coordinates(lng: coordinatesList[0], lat: coordinatesList[1]);
+  }
 }
 
 class Properties {
