2bfdfba13a81ccefccd281993cf1873e5a3167aa
Johannes Bolz <bolz@365farmnet.com>
2020-02-24 16:15:36 +0100

    tap on app icon opens first screen

--- lib/view/main_widget.dart
+++ lib/view/main_widget.dart
@@ -100,10 +100,13 @@ class _MainWidgetState extends State<MainWidget> {
                       ]
                   ),
                 ),
-                leading: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 16.0, top: 8.0, bottom: 8.0),
-                  child: Image.asset('assets/images/ic_logo.png'),
+                leading: InkWell(
+                  onTap: () => _onItemTapped(0),
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 16.0, top: 8.0, bottom: 8.0),
+                    child: Image.asset('assets/images/ic_logo.png'),
+                  ),
                 ),
               ),
               body: versionSupported ? IndexedStack(
